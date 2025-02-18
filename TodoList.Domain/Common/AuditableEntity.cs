5bb28dd8c9b4e9114f006da535845ed30b6a925c
Mohamed Emad <mem@luftborn.com>
2025-02-18 19:27:43 +0200

    Create domain layer

---
+++ TodoList.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoList.Domain.Common;
+
+public abstract class AuditableEntity
+{
+    public DateTimeOffset Created { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? LastModified { get; set; }
+    public DateTimeOffset? Deleted { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+}
---
+++ TodoList.Domain/Entities/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using TodoList.Domain.Common;
+
+namespace TodoList.Domain.Entities;
+
+public class TodoItem : AuditableEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+}
